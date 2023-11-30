3caa3dae24f4f6f949da9c629bf8ca3e6e9ed519
Abd eldjalil <selamnia.a.eldjalil@gmail.com>
2023-11-30 12:11:13 +0100

    header first styling done

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,24 @@
-import { Poppins } from 'next/font/google'
-import Footer from '../components/Footer.jsx'
-import Header from '../components/Header.jsx'
-import './globals.css'
+import { Poppins } from "next/font/google";
+import Footer from "../components/Footer.jsx";
+import Header from "../components/Header.jsx";
+import "./globals.css";
 
-const poppins = Poppins({ weight: '400', subsets: ['latin'] })
+const poppins = Poppins({ weight: "500", subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Travel',
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-}
+  title: "Travel",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html className={poppins.className} lang="en">
-      <Header />
-      <body >{children}</body>
-      <Footer />
+      <body className="bg-white">
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#E5B285",
+        Black: "#231A12",
+        grey: "#BCBAB8",
+        white: "#FFFFFF",
+      },
+      boxShadow: {
+        "3xl": "0px 0px 10px 2px #E5B285",
       },
     },
   },
   plugins: [],
-}
+};

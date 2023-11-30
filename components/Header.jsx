2e7ff3fb5e1e1b65bafec64c7af77703b7e140a4
Abd eldjalil <selamnia.a.eldjalil@gmail.com>
2023-11-30 12:14:22 +0100

    header size fixes

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const Header = () => {
   return (
     <header className="border-b-[1px] bg-white border-b-[#BCBAB840] w-full flex items-center justify-between py-4 px-24 text-Black">
-      <div className="font-[600] text-3xl cursor-default">Travel</div>
-      <ul className="flex gap-14 font-[500] text-xl">
+      <div className="font-[600] text-2xl cursor-default">Travel</div>
+      <ul className="flex gap-14 font-[500] text-lg">
         <li className="cursor-pointer">Home</li>
         <li className="cursor-pointer">Booking</li>
         <li className="cursor-pointer">About Us</li>
         <li className="cursor-pointer">FAQs</li>
       </ul>
       <div className="flex gap-12">
-        <button className="bg-primary text-white text-lg rounded-xl px-6 py-3 shadow-3xl">
+        <button className="bg-primary text-white text-base rounded-xl px-6 py-3 shadow-3xl">
           Book Now
         </button>
-        <button className="font-[500] text-xl">Login</button>
+        <button className="font-[500] text-lg">Login</button>
       </div>
     </header>
   );
